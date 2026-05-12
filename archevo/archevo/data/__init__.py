c9096a45e23fe0f19f06d23c9faa0e9749ebbc23
Manvi234 <80777278+Manvi234@users.noreply.github.com>
2026-05-12 13:10:47 -0400

    Delete archevo directory

--- archevo/archevo/data/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-archevo.data: dataset loading and preprocessing.
-"""
-
-from archevo.data.datamodule import ArchEvoDataModule, DATASET_STATS
-
-__all__ = ['ArchEvoDataModule', 'DATASET_STATS']
--- archevo/archevo/search/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-archevo.search: architecture search algorithms.
-"""
-
-from archevo.search.darts import DARTSSearcher
-from archevo.search.evolutionary import EvolutionarySearcher
-
-__all__ = ['DARTSSearcher', 'EvolutionarySearcher']
